2f5b42330138f1819c98c03c01d9f8b2513cb574
Your Name <zyfxdtn@gmail.com>
2021-01-22 23:39:42 -0800

    sampling done within dbGetQuery

--- R/dbglm.R
+++ R/dbglm.R
@@ -20,8 +20,7 @@ dbsample.tbl_df <-function(tbl, n, N, variables, ... ){
 }
  
 dbsample.tbl_duckdb_connection<- function(tbl, n, N, variables, ... ){
-  a<- dbGetQuery(tbl$src$con, dbplyr::build_sql(con = tbl$src$con, dbplyr::sql(dbplyr::sql_render(tbl))))
-  a[sample(N,n),]
+  dbGetQuery(tbl2$src$con, dbplyr::build_sql(con = tbl2$src$con, dbplyr::build_sql(con = tbl2$src$con, dbplyr::sql(dbplyr::sql_render(tbl2)))), n = n)
 }
 
    
